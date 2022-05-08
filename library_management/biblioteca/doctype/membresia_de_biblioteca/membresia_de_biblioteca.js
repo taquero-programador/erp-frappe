b52a6c1ab779f0ce79e11f1e202ac4ffb7e2728e
taquero-programados <jrangel93@protonmail.com>
2022-05-07 20:32:19 -0500

    feat(doctype): vinculacion de doctypes
    Vinculacion de doctype miembros de libreria hacia membresias

---
+++ library_management/biblioteca/doctype/membresia_de_biblioteca/membresia_de_biblioteca.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Javier Rangel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Membresia de biblioteca', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ library_management/biblioteca/doctype/miembro_de_biblioteca/miembro_de_biblioteca.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Javier Rangel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Miembro de biblioteca', {
+	// refresh: function(frm) {
+
+	// }
+});
